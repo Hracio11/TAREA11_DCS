13aeed35854c35afa777c158bf360b8be27d1a3c
Hubert Aquino <hubertaquino15@gmail.com>
2025-05-20 21:35:33 -0500

    Agregar archivos de proyecto.

---
+++ APPUSERS/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿
+namespace APPUSERS
+{
+    partial class FRMRESULTADO
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvUsuarios = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(12, 94);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(215, 23);
+            this.btnGuardar.TabIndex = 0;
+            this.btnGuardar.Text = "GUARDAR";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(12, 68);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(215, 20);
+            this.txtCorreo.TabIndex = 1;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(12, 29);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(215, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "NOMBRE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "CONTRASEÑA";
+            // 
+            // dgvUsuarios
+            // 
+            this.dgvUsuarios.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvUsuarios.Location = new System.Drawing.Point(233, 13);
+            this.dgvUsuarios.Name = "dgvUsuarios";
+            this.dgvUsuarios.Size = new System.Drawing.Size(402, 114);
+            this.dgvUsuarios.TabIndex = 5;
+            // 
+            // FRMRESULTADO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 138);
+            this.Controls.Add(this.dgvUsuarios);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.btnGuardar);
+            this.Name = "FRMRESULTADO";
+            this.Text = "REGISTRO";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvUsuarios)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgvUsuarios;
+    }
+}
+
---
+++ APPUSERS/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SQLite;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APPUSERS
+{
+    public partial class FRMRESULTADO : Form
+    {
+        string rutaBD = "usuarios.db";
+        string cadenaConexion;
+
+        public FRMRESULTADO()
+        {
+            InitializeComponent();
+            cadenaConexion = $"Data Source={rutaBD};Version=3;";
+            CrearBaseDeDatos();
+            CargarUsuarios();
+        }
+
+
+        private void CrearBaseDeDatos()
+        {
+            if (!File.Exists(rutaBD))
+            {
+                SQLiteConnection.CreateFile(rutaBD);
+
+                using (var conn = new SQLiteConnection(cadenaConexion))
+                {
+                    conn.Open();
+                    string query = @"CREATE TABLE usuarios (
+                                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                    nombre TEXT NOT NULL,
+                                    correo TEXT NOT NULL)";
+                    using (var cmd = new SQLiteCommand(query, conn))
+                    {
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text.Trim();
+            string correo = txtCorreo.Text.Trim();
+
+            if (nombre == "" || correo == "")
+            {
+                MessageBox.Show("Completa ambos campos.");
+                return;
+            }
+
+            using (var conn = new SQLiteConnection(cadenaConexion))
+            {
+                conn.Open();
+                string query = "INSERT INTO usuarios (nombre, correo) VALUES (@nombre, @correo)";
+                using (var cmd = new SQLiteCommand(query, conn))
+                {
+                    cmd.Parameters.AddWithValue("@nombre", nombre);
+                    cmd.Parameters.AddWithValue("@correo", correo);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+
+            txtNombre.Clear();
+            txtCorreo.Clear();
+            CargarUsuarios();
+        }
+
+        private void CargarUsuarios()
+        {
+            using (var conn = new SQLiteConnection(cadenaConexion))
+            {
+                conn.Open();
+                string query = "SELECT * FROM usuarios";
+                using (var da = new SQLiteDataAdapter(query, conn))
+                {
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    dgvUsuarios.DataSource = dt;
+                }
+            }
+        }
+    }
+}
